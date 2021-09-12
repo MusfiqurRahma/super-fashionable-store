9ee884c50769368e0d84172910be7c49f6aeb480
Musfiqur Rahma <musfiq371@gmail.com>
2021-09-13 04:03:33 +0600

    search box added

--- js/app.js
+++ js/app.js
@@ -16,6 +16,8 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <h4>Rating:${product.rating.rate}</h4>
+      <h4>Rating Count:${product.rating.count}</h4>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
